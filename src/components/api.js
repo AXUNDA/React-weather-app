5584808f6e99435979acb358fb42672a348c49de
Axunda <charlesemmanuelazunda@gmail.com>
2022-07-23 10:57:40 +0100

    removed api keys

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,10 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6b06ac3ccdmshca3bd5d1ab188d9p1887c5jsn7531d86fa09b',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': 'your x-rapid apikey',
+		'X-RapidAPI-Host': 'your x rapid api host',
 	}
 };
 export const geoApiUrl= "https://wft-geo-db.p.rapidapi.com/v1/geo"
  export const weatherApiUrl = "https://api.openweathermap.org/data/2.5"
- export const apiKey = "ac4201d1ac49e72e5438d48bf5b50e62"
+ export const apiKey = "Your open weather api key"
