c98ee50e37a96b7eb672b799fe4d3bfd67c4e85d
Axunda <charlesemmanuelazunda@gmail.com>
2022-08-29 00:44:27 +0100

    final

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,10 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'your x-rapid apikey',
-		'X-RapidAPI-Host': 'your x rapid api host',
+		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 export const geoApiUrl= "https://wft-geo-db.p.rapidapi.com/v1/geo"
  export const weatherApiUrl = "https://api.openweathermap.org/data/2.5"
- export const apiKey = "Your open weather api key"
+ export const apiKey = ""
